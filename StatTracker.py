ab88699a6e2b8f301bf9ab13bfa1a824b9b3d284
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-02-10 11:39:45 +1000

    stat track code fixes

--- StatTracker.py
+++ StatTracker.py
@@ -1,16 +1,20 @@
-import ctypes, sys
+import sys
+import ctypes
 
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
-
+        
 if is_admin():
-    print()
+  # Do stuff
+  print()
+  
 else:
     # Re-run the program with admin rights
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    sys.stdout.write('Not running as admin, relaunching...?\n')
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
 
 import os
 os.system('')
@@ -41,7 +45,7 @@ c=""
 gn = 1
 wstreak = 0
 lSa = 0
-lSb = 1
+lSb = 0
 lSc = 0
 
 
@@ -52,7 +56,7 @@ def Start() :
     print("Enter starting team. ")
     print("Type \033[0;34m1 for \033[0;34mLoose Gooses")
     print("\033[0;37mType \033[0;31m2 \033[0;37mfor \033[0;31mWet Willies")
-    print("\033[0;37mType \033[0;32m3 \033[0;37mfor \033[0;32m5 Musketeers\033[0;37m")
+    print("\033[0;37mType \033[0;33m3 \033[0;37mfor \033[0;33m5 Musketeers\033[0;37m")
     starter = int(input("Enter: "))
 
     if((starter != 1) and (starter != 2) and (starter != 3)) :
@@ -62,7 +66,7 @@ def Start() :
     print("Enter other team. ")
     print("Type\033[0;34m 1 \033[0;37mfor \033[34m Loose Gooses")
     print("\033[0;37mType \033[0;31m2 \033[0;37mfor \033[0;31mWet Willies")
-    print("\033[0;37mType \033[0;32m3 \033[0;37mfor \033[0;32m5 Musketeers\033[0;37m")
+    print("\033[0;37mType \033[0;33m3 \033[0;37mfor \033[0;33m5 Musketeers\033[0;37m")
     other = int(input("Enter: "))
     print()
 
@@ -75,25 +79,25 @@ def Start() :
         LG=a
     elif starter == 2 :
         a="\033[0;31mWet Willies\033[0;37m"
-    else : a = "\033[0;32m5 Musketeers\033[0;37m"
+    else : a = "\033[0;33m5 Musketeers\033[0;37m"
 
     if other == 1 :
         b="\033[0;34mLoose Gooses\033[0;37m"
     elif other == 2 :
         b="\033[0;31mWet Willies\033[0;37m"
-    else : b = "\033[0;32m5 Musketeers\033[0;37m"
+    else : b = "\033[0;33m5 Musketeers\033[0;37m"
 
     if (a=="\033[0;34mLoose Gooses\033[0;37m" and b=="\033[0;31mWet Willies\033[0;37m") :
-        c="\033[0;32m5 Musketeers\033[0;37m"
-    elif (a=="\033[0;34mLoose Gooses\033[0;37m" and b=="\033[0;32m5 Musketeers\033[0;37m") :
+        c="\033[0;33m5 Musketeers\033[0;37m"
+    elif (a=="\033[0;34mLoose Gooses\033[0;37m" and b=="\033[0;33m5 Musketeers\033[0;37m") :
         c="\033[0;31mWet Willies\033[0;37m"
     elif (a=="\033[0;31mWet Willies\033[0;37m" and b=="\033[0;34mLoose Gooses\033[0;37m") :
-        c="\033[0;32m5 Musketeers\033[0;37m"
-    elif (a=="\033[0;31mWet Willies\033[0;37m" and b=="\033[0;32m5 Musketeers\033[0;37m") :
+        c="\033[0;33m5 Musketeers\033[0;37m"
+    elif (a=="\033[0;31mWet Willies\033[0;37m" and b=="\033[0;33m5 Musketeers\033[0;37m") :
         c="\033[0;34mLoose Gooses\033[0;37m"
-    elif (a=="\033[0;32m5 Musketeers\033[0;37m" and b=="\033[0;34mLoose Gooses\033[0;37m") :
+    elif (a=="\033[0;33m5 Musketeers\033[0;37m" and b=="\033[0;34mLoose Gooses\033[0;37m") :
         c="\033[0;31mWet Willies\033[0;37m"
-    elif (a=="\033[0;32m5 Musketeers\033[0;37m" and b=="\033[0;31mWet Willies\033[0;37m") :
+    elif (a=="\033[0;33m5 Musketeers\033[0;37m" and b=="\033[0;31mWet Willies\033[0;37m") :
         c="\033[0;34mLoose Gooses\033[0;37m"
 
 
@@ -104,7 +108,7 @@ def Start() :
 
 def TrimColours(text) :
     text = text.replace('\033[0;34m', '')
-    text = text.replace('\033[0;32m', '')
+    text = text.replace('\033[0;33m', '')
     text = text.replace('\033[0;31m', '')
     text = text.replace('\033[0;37m', '')
     return(text)
@@ -140,11 +144,11 @@ def Main() :
         if(winner == a) :
             wstreak += 1
             lSb += 1
+            data.append([gn, TrimColours(winner), TrimColours(loser), scorer, wstreak, lSb])
         else :
             wstreak = 1
-            lSa = 1
-            lSb = 0
-        data.append([gn, TrimColours(winner), TrimColours(loser), scorer, wstreak, lSb])
+            lSb = 1
+            data.append([gn, TrimColours(winner), TrimColours(loser), scorer, wstreak, lSb])
 
 
         savedlsB = lSb
@@ -190,7 +194,7 @@ def Backup() :
         ws.append(i)
 
     name = 'DataBackups/' + str(gn) + '.xlsx'
-    wb.save(os.path.join(name))
+    wb.save(os.path.join('Backup.xlsx'))
 
     print("Backed up.")
 
